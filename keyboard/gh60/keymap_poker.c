00c78f7d6aa9ddf6edf1840a38283ec8eba01000
Kevin Hoogendijk <kevinhoog@gmail.com>
2016-02-13 06:25:02 +0100

    Adds function layer 1

--- keyboard/gh60/keymap_poker.c
+++ keyboard/gh60/keymap_poker.c
@@ -5,9 +5,15 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KEYMAP(ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC, \
          TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC, ENT, \
          CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN, QUOT, NUHS,  \
-         LSFT,NUBS,  Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH, RSFT, \
-         LCTL,LGUI,LALT,          SPC,                     RALT,FN1,RCTL, DEL),
+         FN0 ,NUBS,  Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH, RSFT, \
+         LCTL,LGUI,LALT,          SPC,                     RALT,RGUI,FN1, RCTL),
+
+  KEYMAP(MUTE, F1,   F2,   F3,   F4,   F5,   F6,   F7,   F8,   F9,   F10,  F11,  F12, DEL, \
+        TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, PGUP, UP,   TRNS,  \
+        TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, MYCM, TRNS, TRNS, PGDN, LEFT, RGHT, TRNS,  \
+        TRNS, TRNS, TRNS, CALC, TRNS, TRNS, TRNS, TRNS, HOME, END,  DOWN, TRNS, TRNS,  \
+        LGUI, TRNS, TRNS,             TRNS,                   PSCR, RCTL, TRNS, TRNS),
 };
 const uint16_t PROGMEM fn_actions[] = {
-
+        [0] = ACTION_LAYER_MOMENTARY(1),
 };
