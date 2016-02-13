1814d6a1beaa7ca1fbfab3306fc543e303c02cdb
Kevin Hoogendijk <kevinhoog@gmail.com>
2016-02-13 06:28:53 +0100

    Fixes tabs on keymap

--- keyboard/gh60/keymap_poker.c
+++ keyboard/gh60/keymap_poker.c
@@ -2,11 +2,11 @@
 
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* 0: qwerty */
-  KEYMAP(ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC, \
-         TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC, ENT, \
-         CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN, QUOT, NUHS,  \
-         FN0 ,NUBS,  Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH, RSFT, \
-         LCTL,LGUI,LALT,          SPC,                     RALT,RGUI,FN1, RCTL),
+  KEYMAP(ESC,   1,    2,   3,   4,   5,   6,   7,   8,   9,   0,    MINS,EQL, BSPC, \
+         TAB,   Q,    W,   E,   R,   T,   Y,   U,   I,   O,   P,    LBRC,RBRC,ENT, \
+         CAPS,  A,    S,   D,   F,   G,   H,   J,   K,   L,   SCLN, QUOT,NUHS,  \
+         FN0 ,  NUBS, Z,   X,   C,   V,   B,   N,   M,   COMM,DOT,  SLSH,RSFT, \
+         LCTL,  LGUI, LALT,          SPC,                     RALT, RGUI,FN1, RCTL),
 
   KEYMAP(MUTE, F1,   F2,   F3,   F4,   F5,   F6,   F7,   F8,   F9,   F10,  F11,  F12, DEL, \
         TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, PGUP, UP,   TRNS,  \
