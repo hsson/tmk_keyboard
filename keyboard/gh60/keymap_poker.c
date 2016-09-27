28dc9f92acef5c9d6e7025773fb160c203a35602
Criticalstone <kevinhoog@gmail.com>
2016-09-27 19:41:10 +0200

    Removes unnessecary layer

--- keyboard/gh60/keymap_poker.c
+++ keyboard/gh60/keymap_poker.c
@@ -6,22 +6,14 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          TAB,   Q,    W,   E,   R,   T,   Y,   U,   I,   O,   P,    LBRC,RBRC,ENT,  \
          CAPS,  A,    S,   D,   F,   G,   H,   J,   K,   L,   SCLN, QUOT,NUHS,      \
          FN0 ,  NUBS, Z,   X,   C,   V,   B,   N,   M,   COMM,DOT,  SLSH,RSFT,      \
-         LCTL,  LGUI, LALT,          SPC,                     RALT,RGUI,FN1, RCTL),
+         LCTL,  LGUI, LALT,          SPC,                     FN0,  RALT,RGUI, RCTL),
 
   KEYMAP(MUTE,  F1,     F2,   F3,   F4,   F5,   F6,   F7,   F8,   F9,   F10,  F11,  F12,  DEL, \
-         TRNS,  TRNS,   TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,  \
-         TRNS,  TRNS,   TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, MPRV, MPLY, MNXT,      \
-         TRNS,  TRNS,   TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,      \
-         TRNS,  TRNS,   TRNS,             TRNS,                         PSCR, TRNS, TRNS, TRNS),
-
-  KEYMAP(TRNS,  TRNS,   TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,  \
          TRNS,  TRNS,   UP,   TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,  \
-         TRNS,  LEFT,   DOWN, RIGHT, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,      \
+         TRNS,  LEFT,   DOWN, RIGHT, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, MPRV, MPLY, MNXT,      \
          TRNS,  TRNS,   TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,      \
-         TRNS,  TRNS,   TRNS,             TRNS,                         PSCR, TRNS, TRNS, TRNS),
+         TRNS,  TRNS,   TRNS,             TRNS,                         TRNS, PSCR, TRNS, TRNS),
 };
 const uint16_t PROGMEM fn_actions[] = {
         [0] = ACTION_LAYER_MOMENTARY(1),
-        [1] = ACTION_LAYER_TAP_KEY(2, KC_ESC),
-        [2] = ACTION_LAYER_MOMENTARY(2),
 };
