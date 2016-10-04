41fb9127fa0f318b913e978b1e2898a51adbcebd
Alexander Håkansson <al3xand3r.h@gmail.com>
2016-10-04 13:07:56 +0200

    Swaps positions for the END and Page Up/Down keys

--- keyboard/gh60/keymap_poker.c
+++ keyboard/gh60/keymap_poker.c
@@ -4,9 +4,9 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* 0: qwerty */
   KEYMAP(ESC, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10,   F11,    F12,  INS,  DEL, \
          GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,     MINS,   EQL,  BSPC, HOME, \
-         TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,     LBRC,   RBRC,       PGUP, \
-         CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,  QUOT,   NUHS, ENT,  PGDN,  \
-         LSFT,NUBS,  Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,   RSFT, UP,   END,  \
+         TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,     LBRC,   RBRC,       END, \
+         CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,  QUOT,   NUHS, ENT,  PGUP,  \
+         LSFT,NUBS,  Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,   RSFT, UP,   PGDN,  \
          LCTL,LGUI,LALT,          SPC,                     RALT,FN0,RCTL, LEFT, DOWN, RGHT),
  /* 1: qwerty */
  KEYMAP(TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, PSCR, \
